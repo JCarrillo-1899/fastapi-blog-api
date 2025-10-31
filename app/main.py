97f185a6a7935e352a795699ca62afade366df51
José Carlos <jccarrilloperez1899@gmail.com>
2025-10-31 15:27:51 -0400

    Modificando main para crear las tablas de la base de datos Blog

--- app/main.py
+++ app/main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel, create_engine
+
+from models.user import User
+from models.post import Post
+from models.comment import Comment
+
+# Creando las tablas
+database_url = "postgresql://postgres:postgres@localhost:5432/Blog"
+engine = create_engine(database_url, echo=True)
+def create_tables():
+    SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 
